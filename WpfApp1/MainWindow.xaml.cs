3eb5589de7a72952578e8963616bc1556bb590bb
Narykov <Narykov@DESKTOP-HHF4TOO>
2024-06-24 17:01:06 +0300

    Add project files.

---
+++ WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            SetUpGame();
+        }
+
+        private void SetUpGame()
+        {
+            List<string> AnimalEmoji = new List<string>()
+            {
+                "🐶", "🐱", "🐭", "🐹", "🐰", "🦊", "🐻", "🐼",
+                "🦁", "🐯", "🐨", "🐼", "🐷", "🐸", "🐵", "🐔"
+            };
+
+            Random random = new Random();
+
+            foreach (TextBlock textblock in mainGrid.Children.OfType<TextBlock>())
+            {
+              
+
+                int index = random.Next(AnimalEmoji.Count);
+                string nextEmoji = AnimalEmoji[index];
+                textblock.Text = nextEmoji;
+                AnimalEmoji.RemoveAt(index);
+            }
+        }
+    }
+}
